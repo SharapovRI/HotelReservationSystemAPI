09ca3f4de308b78cf92941dd55a146e8b68754de
SharapovRI <79543052+SharapovRI@users.noreply.github.com>
2021-10-19 22:48:36 +0300

    Added migrations

---
+++ HotelReservationSystemAPI.Data/Migrations/20211019194332_Initial.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace HotelReservationSystemAPI.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "additional_services",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_additional_services", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "hotels",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    country = table.Column<string>(type: "text", nullable: true),
+                    city = table.Column<string>(type: "text", nullable: true),
+                    address = table.Column<string>(type: "text", nullable: true),
+                    name = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_hotels", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "roles",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_roles", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "types_of_rooms",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: true),
+                    amount_of_seats = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_types_of_rooms", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "costs_of_services",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    hotel_id = table.Column<int>(type: "integer", nullable: false),
+                    additional_services_id = table.Column<int>(type: "integer", nullable: false),
+                    cost = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_costs_of_services", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_costs_of_services_additional_services_additional_services_id",
+                        column: x => x.additional_services_id,
+                        principalTable: "additional_services",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_costs_of_services_hotels_hotel_id",
+                        column: x => x.hotel_id,
+                        principalTable: "hotels",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "persons",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    login = table.Column<string>(type: "text", nullable: true),
+                    password = table.Column<string>(type: "text", nullable: true),
+                    role_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_persons", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_persons_roles_role_id",
+                        column: x => x.role_id,
+                        principalTable: "roles",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "cost_of_rooms",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    hotel_id = table.Column<int>(type: "integer", nullable: false),
+                    type_id = table.Column<int>(type: "integer", nullable: false),
+                    cost = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_cost_of_rooms", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_cost_of_rooms_hotels_hotel_id",
+                        column: x => x.hotel_id,
+                        principalTable: "hotels",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_cost_of_rooms_types_of_rooms_type_id",
+                        column: x => x.type_id,
+                        principalTable: "types_of_rooms",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "rooms",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    hotel_id = table.Column<int>(type: "integer", nullable: false),
+                    type_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_rooms", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_rooms_hotels_hotel_id",
+                        column: x => x.hotel_id,
+                        principalTable: "hotels",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_rooms_types_of_rooms_type_id",
+                        column: x => x.type_id,
+                        principalTable: "types_of_rooms",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "orders",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    room_id = table.Column<int>(type: "integer", nullable: false),
+                    check_in_time = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    check_out_time = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    cost = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_orders", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_orders_rooms_room_id",
+                        column: x => x.room_id,
+                        principalTable: "rooms",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "additional_services_in_orders",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    order_id = table.Column<int>(type: "integer", nullable: false),
+                    addition_service_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_additional_services_in_orders", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_additional_services_in_orders_additional_services_addition_~",
+                        column: x => x.addition_service_id,
+                        principalTable: "additional_services",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_additional_services_in_orders_orders_order_id",
+                        column: x => x.order_id,
+                        principalTable: "orders",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_additional_services_in_orders_addition_service_id",
+                table: "additional_services_in_orders",
+                column: "addition_service_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_additional_services_in_orders_order_id",
+                table: "additional_services_in_orders",
+                column: "order_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_cost_of_rooms_hotel_id",
+                table: "cost_of_rooms",
+                column: "hotel_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_cost_of_rooms_type_id",
+                table: "cost_of_rooms",
+                column: "type_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_costs_of_services_additional_services_id",
+                table: "costs_of_services",
+                column: "additional_services_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_costs_of_services_hotel_id",
+                table: "costs_of_services",
+                column: "hotel_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_orders_room_id",
+                table: "orders",
+                column: "room_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_persons_role_id",
+                table: "persons",
+                column: "role_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_rooms_hotel_id",
+                table: "rooms",
+                column: "hotel_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_rooms_type_id",
+                table: "rooms",
+                column: "type_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "additional_services_in_orders");
+
+            migrationBuilder.DropTable(
+                name: "cost_of_rooms");
+
+            migrationBuilder.DropTable(
+                name: "costs_of_services");
+
+            migrationBuilder.DropTable(
+                name: "persons");
+
+            migrationBuilder.DropTable(
+                name: "orders");
+
+            migrationBuilder.DropTable(
+                name: "additional_services");
+
+            migrationBuilder.DropTable(
+                name: "roles");
+
+            migrationBuilder.DropTable(
+                name: "rooms");
+
+            migrationBuilder.DropTable(
+                name: "hotels");
+
+            migrationBuilder.DropTable(
+                name: "types_of_rooms");
+        }
+    }
+}
---
+++ HotelReservationSystemAPI.Data/NpgsqlContext.cs
@@ -0,0 +1,27 @@
+﻿using HotelReservationSystemAPI.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservationSystemAPI.Data
+{
+    public class NpgsqlContext : DbContext //-Project HotelReservationSystemAPI.Data
+    {
+        public DbSet<AdditionalService> additional_services { get; set; }
+        public DbSet<AdditionalServicesInOrder> additional_services_in_orders { get; set; }
+        public DbSet<CostOfRooms> cost_of_rooms { get; set; }
+        public DbSet<CostsOfServices> costs_of_services { get; set; }
+        public DbSet<Hotel> hotels { get; set; }
+        public DbSet<Order> orders { get; set; }
+        public DbSet<Person> persons { get; set; }
+        public DbSet<Role> roles { get; set; }
+        public DbSet<Room> rooms { get; set; }
+        public DbSet<TypesOfRooms> types_of_rooms { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=HotelReservationSystemAPI_DB;Username=postgres;Password=PostgreSQL");
+        }
+    }
+}
